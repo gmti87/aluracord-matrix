2eb15e2238933ffff00aad84164b767dff12ceb7
gmti87 <gmoreti87@gmail.com>
2022-01-25 21:13:34 -0300

    Segunda aula Imersão React Alura

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d65519f73d5cadc2"; }
+/******/ 		__webpack_require__.h = function() { return "d4f267076a0eaa13"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,7 @@
 import { Box, Button, Text, TextField, Image } from '@skynexui/components';
+import React from 'react';
+import { useRouter } from 'next/router';
 import appConfig from '../config.json'
-
-function GlobalStyle() {
-    return (
-      <style global jsx>{`
-        * {
-          margin: 0;
-          padding: 0;
-          box-sizing: border-box;
-          list-style: none;
-        }
-        body {
-          font-family: 'Open Sans', sans-serif;
-        }
-        /* App fit Height */ 
-        html, body, #__next {
-          min-height: 100vh;
-          display: flex;
-          flex: 1;
-        }
-        #__next {
-          flex: 1;
-        }
-        #__next > * {
-          flex: 1;
-        }
-        /* ./App fit Height */ 
-      `}</style>
-    );
-}
 
 function Title( props ) {
     //console.log(props);
@@ -61,11 +34,12 @@ function Title( props ) {
 // export default HomePage
 
 export default function PaginaInicial() {
-    const username = 'gmti87';
-  
+    // const username = 'gmti87';
+    const [username, setUsername] = React.useState('gmti87');
+    const roteamento = useRouter(); 
+    
     return (
       <>
-        <GlobalStyle />
         <Box
           styleSheet={{
             display: 'flex', alignItems: 'center', justifyContent: 'center',
@@ -92,6 +66,12 @@ export default function PaginaInicial() {
             {/* Formulário */}
             <Box
               as="form"
+              onSubmit={function (infosDoEvento) {
+                infosDoEvento.preventDefault();
+                console.log("Alguem submeteu o form")
+                console.log(roteamento);
+                // window.location.href = '/chat';
+              }}
               styleSheet={{
                 display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
                 width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
@@ -101,9 +81,33 @@ export default function PaginaInicial() {
               <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals[300] }}>
                 {appConfig.name}
               </Text>
-  
+
+              {/*
+              <input
+                type="text"
+                value={username}
+                onChange={function handler(event) {
+                  console.log('Usario digittou', event.target.value);
+                  // onde esta o valor
+                  const valor = event.target.value;
+                  // trocar o valor da variavel
+                  // Atraves do react e avise quem precisa
+                  setUsername(valor);
+                }}
+              />
+              */}
+              
               <TextField
-                fullWidth
+                value={username}
+                onChange={function handler(event) {
+                  console.log('Usario digittou', event.target.value);
+                  // onde esta o valor
+                  const valor = event.target.value;
+                  // trocar o valor da variavel
+                  // Atraves do react e avise quem precisa
+                  setUsername(valor);
+                }}
+                fllWidth
                 textFieldColors={{
                   neutral: {
                     textColor: appConfig.theme.colors.neutrals[200],
